0b3f7d28583d1a10fbef308829310dd2de97687f
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2023-05-03 15:53:04 +0100

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -92,8 +92,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
-# html_theme = 'alabaster'
+# html_theme = 'bizstyle'
+html_theme = 'alabaster'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
