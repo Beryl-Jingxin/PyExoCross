e63a157b02485ad0754f27b6a7bd01c6063f168d
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2023-05-03 18:06:41 +0100

    Upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,13 +36,18 @@ version = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables',
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'recommonmark',
-    'sphinx_markdown_tables',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode'
 ]
 
 intersphinx_mapping = {
@@ -68,7 +73,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
