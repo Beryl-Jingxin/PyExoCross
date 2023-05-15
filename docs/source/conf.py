aca0b94c09a6889761377a9eff16ca29034cd04b
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2023-05-15 18:07:20 +0100

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages'
+    'sphinx.ext.githubpages',
     'sphinx.ext.napoleon',
 ]
 
