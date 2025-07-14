d17f08d336c171f0bba1c568a9e793154fbcec0a
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2025-07-14 12:15:16 +0100

    update Doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,11 @@ extensions = [
     'sphinx.ext.napoleon',
 ]
 
+myst_enable_extensions = [
+    "dollarmath",
+    "amsmath",
+]
+
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
