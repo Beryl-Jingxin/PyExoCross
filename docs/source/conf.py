8f354b44b6a9fb37690b18659f36a86ebc6bb678
Jingxin Zhang <jingxin.zhang.19@ucl.ac.uk>
2025-07-14 12:32:10 +0100

    update Doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,6 +128,9 @@ epub_show_urls = 'footnote'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'table-center.css',
+]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
